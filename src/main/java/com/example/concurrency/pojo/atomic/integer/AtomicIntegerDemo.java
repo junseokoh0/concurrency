36ff3447cc4de03f0614c8ff79213310105c6f55
junseokoh0 <junseok@bodycodi.com>
2022-12-12 08:58:18 +0900

    ThreadLocal Simple 예제 코드 추가

--- src/main/java/com/example/concurrency/pojo/atomic/integer/AtomicIntegerDemo.java
+++ src/main/java/com/example/concurrency/pojo/atomic/integer/AtomicIntegerDemo.java
@@ -9,7 +9,8 @@ public class AtomicIntegerDemo {
     public static void main(String[] args) throws InterruptedException {
         System.out.println(">>>>> runtime availableProcessors :: " + Runtime.getRuntime().availableProcessors());
 //        final Counter counter = new Counter();
-        final AtomicCounter counter = new AtomicCounter();
+//        final AtomicCounter counter = new AtomicCounter();
+        final ThreadLocalCounter counter = new ThreadLocalCounter();
         final Thread t1 = new Thread(counter, "t1");
         final Thread t2 = new Thread(counter, "t2");
         t1.start();
@@ -30,6 +31,33 @@ class AtomicCounter implements Runnable {
             Thread.sleep(10);
 //            System.out.println(">>>>> Thread [" + Thread.currentThread().getName() + "] current count :: " + getCount());
             count.addAndGet(1);
+        }
+        printThreadWithCount(Thread.currentThread());
+    }
+
+    private void printThreadWithCount(Thread thread) {
+        System.out.println(">>>>> " + thread.getName() + " - count : " + getCount());
+    }
+
+    public int getCount() {
+        return count.get();
+    }
+}
+
+class ThreadLocalCounter implements Runnable {
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 10;
+
+    private ThreadLocal<Integer> count = new ThreadLocal<>();
+
+    @SneakyThrows
+    @Override
+    public void run() {
+
+        if (START_INDEX == 0) count.set(0);
+
+        for (int i = START_INDEX; i < END_INDEX; i++) {
+            count.set(count.get() + 1);
         }
         printThreadWithCount(Thread.currentThread());
     }
