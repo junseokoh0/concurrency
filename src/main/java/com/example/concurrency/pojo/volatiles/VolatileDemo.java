dec8a89be831676e2739dc86e3e835aa5b98451f
junseok <junseok@bodycodi.com>
2022-12-12 14:07:46 +0900

    Volatile 이해를 위한 코드 작성 중

--- src/main/java/com/example/concurrency/pojo/volatiles/VolatileDemo.java
+++ src/main/java/com/example/concurrency/pojo/volatiles/VolatileDemo.java
@@ -1,6 +1,50 @@
 package com.example.concurrency.pojo.volatiles;
 
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * TODO volatile 변수 C에 대한 정확한 이해와 설명 필요
+ */
 public class VolatileDemo {
+
+    @Getter @Setter
+    public static class Shared {
+        private int a;
+        private int b;
+        private volatile int c;
+
+        public void display() {
+            System.out.println(getC());
+            System.out.println(getB());
+            System.out.println(getA());
+        }
+
+        public void setSharedField(){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            setA(1);
+            setB(2);
+            setC(3);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final Shared shared = new Shared();
+
+        Thread thread1 = new Thread(shared::setSharedField);
+        Thread thread2 = new Thread(shared::display);
+        thread1.start();
+        thread2.start();
+    }
+
+
 
 
 }
+
+
+
