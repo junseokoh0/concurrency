250bea630bdb7db4b44edaea60eb333391fbc607
junseok <junseok@bodycodi.com>
2022-12-14 20:51:01 +0900

    Volatile 속도 테스트

---
+++ src/main/java/com/example/concurrency/pojo/volatiles/VolatileTest.java
@@ -0,0 +1,33 @@
+package com.example.concurrency.pojo.volatiles;
+
+public class VolatileTest {
+
+    public static void main(String[] args) {
+        final SharedObject sharedObject = new SharedObject();
+
+        final Thread thread1 = new Thread(() -> {
+            final long startTimeMillis = System.currentTimeMillis();
+            for (int i = 0; i < 100000000; i++) sharedObject.counter++;
+            final long endTimeMillis = System.currentTimeMillis();
+            System.out.println(">>>>> executeTimeMillis :: " + (endTimeMillis - startTimeMillis));
+            System.out.println(":::::" + Thread.currentThread().getName() + " counter :: " + sharedObject.getCounter());
+        });
+//        final Thread thread2 = new Thread(() -> {
+//            for (int i = 0; i < 100000000; i++) sharedObject.counter++;
+//            System.out.println(":::::" + Thread.currentThread().getName() + " counter :: " + sharedObject.getCounter());
+//        });
+
+        thread1.start();
+//        thread2.start();
+        System.out.println(":::::" + Thread.currentThread().getName() + " counter :: " + sharedObject.getCounter());
+    }
+}
+
+class SharedObject {
+    public volatile int counter = 0;
+
+
+    public int getCounter() {
+        return counter;
+    }
+}
